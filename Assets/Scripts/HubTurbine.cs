7d8c2221ab41420c1887bd781fbe2de72670daff
Thomas Turner <tt792@york.ac.uk>
2022-02-23 15:06:53 +0000

    Updated hub turbines with non script references

--- Assets/Scripts/HubTurbine.cs
+++ Assets/Scripts/HubTurbine.cs
@@ -14,6 +14,9 @@ public class HubTurbine : WindTurbine
     public GameObject delivary_drone_prefab;
     public GameObject maintenance_drone_prefab;
 
+    //list of the drones this hub contains
+    public List<GameObject> dronesUnderControl;
+
     
     // Control things
     void Start()
@@ -21,12 +24,31 @@ public class HubTurbine : WindTurbine
         // TODO: Remove
         jobs_queue.Add(new Job("A31", JobType.Scan, 0, 0));
 
-        GameObject obj = Instantiate(
-                delivary_drone_prefab, 
-                this.transform.position + new Vector3(0, 105, 0), 
-                Quaternion.identity
-            );
-        pad.holdChargingDrone(obj.GetComponent<Drone>(), 0);
+        /*
+        would probably have a start script which tells the hub turbines to create all the drones
+        then can store them in a list of drones for reference and knowledge of which ones this
+        hub controls
+
+        for now create the drones and store here in this list
+        */
+        GameObject drone1 = Instantiate(
+            delivary_drone_prefab, 
+            this.transform.position + new Vector3(0, 105, 0),
+            Quaternion.identity
+        );
+
+        dronesUnderControl.Add(drone1);
+
+        GameObject drone2 = Instantiate(
+            delivary_drone_prefab, 
+            this.transform.position + new Vector3(0, 115, 0),
+            Quaternion.identity
+        );
+
+        dronesUnderControl.Add(drone2);
+
+        pad.holdChargingDrone(dronesUnderControl[0], 0);
+        pad.holdChargingDrone(dronesUnderControl[1], 1);
     }
 
     void Update()
